41da62ab370288e147cfaba7ac68be8276df1ae2
MasakiYoshida39 <149563224+MasakiYoshida39@users.noreply.github.com>
2025-05-31 17:42:31 +0900

    doGet作成

--- src/main/java/control/HelloServlet.java
+++ src/main/java/control/HelloServlet.java
@@ -1,15 +1,18 @@
 package control;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 /**
  * Servlet implementation class HelloServlet
  */
+//URLマッピング
 @WebServlet("/hellos")
 public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -18,8 +21,26 @@ public class HelloServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		        //レスポンスのコンテンツタイプおよびエンコーディング方式を指定
+				response.setContentType("text/html; charset=UTF-8");
+
+				//レスポンス書き出し用オブジェクトの取得
+				PrintWriter out = response.getWriter();
+
+				//レスポンスの書き出し
+				out.println("<!DOCTYPE html>                                 ");
+				out.println("<html>                                          ");
+				out.println("<head>                                          ");
+				out.println("<meta charset=\"UTF-8\">                        ");
+				out.println("<title>First Servlet</title>                    ");
+				out.println("<link rel=\"stylesheet\" href=\"style.css\">    ");
+				out.println("</head>                                         ");
+				out.println("<body>                                          ");
+				out.println("    <h1>サーブレット</h1>                       ");
+				out.println("    <h2>最初の表示</h2>                         ");
+				out.println("    <p>Hello Servlet !</p>                      ");
+				out.println("</body>                                         ");
+				out.println("</html>     ");
 	}
 
 	/**
